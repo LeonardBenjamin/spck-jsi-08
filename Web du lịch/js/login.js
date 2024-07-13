785885d68febe228d23366a020ca0346b9ae9f6c
Kai Aetherweigh <136946134+LeonardBenjamin@users.noreply.github.com>
2024-07-13 15:39:16 +0700

    Update login.js

--- Web du lịch/js/login.js
+++ Web du lịch/js/login.js
@@ -1,5 +1,5 @@
 import app from './app.js';
-import Register from '../js/register.js';
+import Register from './register.js/index.js';
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js"
 class Login {
     $txtEmail
